bc3ec3eb48985fdb39d854e9087b60fb036a3e9e
Armaan Chahal <armaan.c2009@gmail.com>
2025-03-30 14:01:08 -0500

    Add GithubCopilotChat plugin. Open with :CopilotChat

--- lua/jomskarlsefni/plugins/actions_preview.lua
+++ lua/jomskarlsefni/plugins/actions_preview.lua
@@ -1,6 +1,6 @@
 return {
     "aznhe21/actions-preview.nvim",
     config = function()
-        vim.keymap.set( {"v", "n"}, "<a-cr>", require("actions-preview").code_actions)
+        vim.keymap.set({ "v", "n" }, "<a-cr>", require("actions-preview").code_actions)
     end,
 }
