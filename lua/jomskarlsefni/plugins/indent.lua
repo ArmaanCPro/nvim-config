11fb06f3e4926d8ac8ae485b85f7fdba241d96ac
Armaan Chahal <armaan.c2009@gmail.com>
2025-03-29 23:22:52 -0500

    add some plugins (trouble, alpha, indent blanklines)

---
+++ lua/jomskarlsefni/plugins/indent.lua
@@ -0,0 +1,18 @@
+return {
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    ---@module "ibl"
+    ---@type ibl.config
+    opts = {},
+
+    config = function()
+        require("ibl").setup({
+            indent = {
+                char = "▏",
+            },
+            scope = {
+                enabled = false,
+            },
+        })
+    end,
+}
