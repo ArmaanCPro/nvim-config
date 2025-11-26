686a14ca4cf71a3780d6291a8b8101755786ed16
Armaan Chahal <armaan.c2009@gmail.com>
2025-11-26 13:57:46 -0600

    move /lsp to /after/lsp to force custom configs to be loaded AFTER nvim-lspconfig default configs

---
+++ after/lsp/clangd.lua
@@ -0,0 +1,33 @@
+-- FROM nvim-lspconfig /lsp/clangd.lua
+
+local function switch_source_header(bufnr, client)
+  local method_name = 'textDocument/switchSourceHeader'
+  ---@diagnostic disable-next-line:param-type-mismatch
+  if not client or not client:supports_method(method_name) then
+    return vim.notify(('method %s is not supported by any servers active on the current buffer'):format(method_name))
+  end
+  local params = vim.lsp.util.make_text_document_params(bufnr)
+  ---@diagnostic disable-next-line:param-type-mismatch
+  client:request(method_name, params, function(err, result)
+    if err then
+      error(tostring(err))
+    end
+    if not result then
+      vim.notify('corresponding file cannot be determined')
+      return
+    end
+    vim.cmd.edit(vim.uri_to_fname(result))
+  end, bufnr)
+end
+
+--@type vim.lsp.Config
+return {
+  on_attach = function(client, bufnr)
+    vim.api.nvim_buf_create_user_command(bufnr, 'LspClangdSwitchSourceHeader', function()
+      switch_source_header(bufnr, client)
+    end, { desc = 'Switch between source/header' })
+
+    vim.keymap.set("n", "<A-o>", "<cmd>LspClangdSwitchSourceHeader<CR>", { noremap = true, silent = true, buffer = bufnr })
+  end,
+}
+
