c590b10018ea589025527ef1b8dcae22c764429c
Armaan Chahal <armaan.c2009@gmail.com>
2025-11-28 01:55:45 -0600

    use treesitter for indentation

--- lua/jomskarlsefni/init.lua
+++ lua/jomskarlsefni/init.lua
@@ -50,6 +50,7 @@ autocmd('FileType', {
     callback = function()
         vim.treesitter.start()
         vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr"
     end,
 })
 
