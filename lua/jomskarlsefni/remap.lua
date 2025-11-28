6e0490f1c0dc6b083f715849feba5fb899bf6e9d
Armaan Chahal <armaan.c2009@gmail.com>
2025-11-28 12:36:35 -0600

    add mappings for moving lines up/down and remap visual block mode to <C-b>

--- lua/jomskarlsefni/remap.lua
+++ lua/jomskarlsefni/remap.lua
@@ -21,3 +21,12 @@ vim.keymap.set("n", "<C-j>", "<C-w>j")
 vim.keymap.set("n", "<C-k>", "<C-w>k")
 vim.keymap.set("n", "<C-l>", "<C-w>l")
 
+-- Move current line or visual selection up/down
+vim.keymap.set("n", "<A-j>", ":m .+1<CR>==") -- move line up(n)
+vim.keymap.set("n", "<A-k>", ":m .-2<CR>==") -- move line down(n)
+vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv") -- move line up(v)
+vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv") -- move line down(v)
+
+-- remap visual block mode to ctrl+b as some terminals override ctrl+v to be paste
+vim.keymap.set("n", "<C-b>", "<C-v>", { desc = "Enter visual block mode" })
+
