881a17899948eaf23491b48e85109e7c88dd40ef
Armaan Chahal <armaan.c2009@gmail.com>
2025-12-10 19:11:28 -0600

    disable some plugins from snacks

--- lua/jomskarlsefni/plugins/snacks.lua
+++ lua/jomskarlsefni/plugins/snacks.lua
@@ -4,18 +4,18 @@ return {
     lazy = false,
     ---@type snacks.Config
     opts = {
-        bigfile = { enabled = true },
         animate = { enabled = true },
         dim = { enabled = true },
         notifier = { enabled = true },
-        quickfile = { enabled = true },
-        scroll = { enabled = true },
         zen = { enabled = true },
 
         terminal = { enabled = false },
         image = { enabled = false },
         lazygit = { enabled = false },
         toggle = { enabled = false },
+        bigfile = { enabled = false },
+        quickfile = { enabled = false },
+        scroll = { enabled = false },
     },
 
     keys = {
